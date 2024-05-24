58dce30cff4c63bb19f25eb7268712b147be3ef4
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-24 16:33:07 +0200

    Add author and URL to blog posts and update display
    The blog posts now include an author and a URL. These have been added to the data structure and are displayed in the frontend. Additionally, CSS styles for the blog post display have been added to improve the layout and presentation of information.

--- src/components/blog/data.tsx
+++ src/components/blog/data.tsx
@@ -1,17 +1,23 @@
 export type blogPost = {
     title: string,
+    author: string,
     summary: string,
-    image?: string
+    image?: string,
+    url: string,
 };
 export type blogPosts = blogPost[];
 
 const post1: blogPost = {
     title: "React with Spring Boot",
+    author: "Florian Weber",
     summary: "\"React with Spring Boot\" explains how to integrate React and Spring Boot to create modern full-stack web applications. React is used for developing dynamic user interfaces, while Spring Boot provides robust backend APIs. The content covers configuring both frameworks, enabling communication between frontend and backend, and best practices for efficient development and maintenance.",
-    image: ""
+    image: "https://fullscale.io/wp-content/uploads/2024/02/spring-boot-and-react-js.png",
+    url: "/blog/react-with-spring-boot"
 }
 const post2: blogPost = {
     title: "React vs. Angular",
+    author: "Mickey Maus",
     summary: "\"React vs. Angular\" explains the differences between React and Angular for building web applications. React, a library by Facebook, offers flexibility and a component-based architecture, while Angular, a framework by Google, provides a comprehensive solution with built-in tools and strong typing. The comparison covers performance, learning curve, community support, and use cases to help developers choose the right tool for their projects.",
+    url: "/blog/react-vs-angular"
 }
 export const blogPosts: blogPosts = [post1, post2];
--- src/components/blogpost/BlogPost.tsx
+++ src/components/blogpost/BlogPost.tsx
@@ -1,4 +1,5 @@
 import {blogPost} from "../blog/data.tsx";
+import "./BlogPost.css"
 
 type Props = {
     blogPost: blogPost
@@ -10,11 +11,19 @@ export default function BlogPost({blogPost}: Props) {
             <h3>
                 {blogPost.title}
             </h3>
-            <div className="blogpost__image">
-                {blogPost.image ? <img src="{blogPost.image}" alt={blogPost.title}/> : null}
-            </div>
-            <div className="blogpost__summary">
-                <p>{blogPost.summary}</p>
+            <p className="blogpost__author">by {blogPost.author}</p>
+            <div className="blogpost__preview-container">
+                <div className="blogpost__image">
+                    {blogPost.image ?
+                        <img src={blogPost.image} alt={blogPost.title} width={400}/> :
+                        <img src="" alt="No image available" width={400}/> }
+                </div>
+                <div className="blogpost__summary">
+                    <p>{blogPost.summary}</p>
+                    <p>
+                        <a href={"blogPost.url"}>Read more</a>
+                    </p>
+                </div>
             </div>
         </article>
     )
