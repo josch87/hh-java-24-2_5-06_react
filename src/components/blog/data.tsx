b60e9bc75326e6ab257e276afc5b5de5de34bce2
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-26 07:36:49 +0200

    Update blog post component and data structure
    Enhanced the blog post component with improved CSS styling, and adjusted the data structure to include the author URL for linking. The changes mainly focus on adding more styles to the blog post elements and revising the data structure for blog posts to enable author linking. This makes the blog posts more visually appealing and allows users to view more details about the author.

--- src/components/blog/data.tsx
+++ src/components/blog/data.tsx
@@ -1,7 +1,7 @@
 export type blogPost = {
     id: number,
     title: string,
-    author: string,
+    author: {name: string, url: string},
     summary: string,
     image?: string,
     url: string,
@@ -11,7 +11,9 @@ export type blogPosts = blogPost[];
 const post1: blogPost = {
     id: 1,
     title: "React with Spring Boot",
-    author: "Florian Weber",
+    author: {
+        name: "Florian Weber",
+        url: "/author/florian-weber",},
     summary: "\"React with Spring Boot\" explains how to integrate React and Spring Boot to create modern full-stack web applications. React is used for developing dynamic user interfaces, while Spring Boot provides robust backend APIs. The content covers configuring both frameworks, enabling communication between frontend and backend, and best practices for efficient development and maintenance.",
     image: "src/assets/spring-boot-and-react-js.png",
     url: "/blog/react-with-spring-boot"
@@ -19,7 +21,10 @@ const post1: blogPost = {
 const post2: blogPost = {
     id: 2,
     title: "React vs. Angular",
-    author: "Mickey Mouse",
+    author: {
+        name: "Mickey Mouse",
+        url: "/author/mickey-mouse",
+    },
     summary: "\"React vs. Angular\" explains the differences between React and Angular for building web applications. React, a library by Facebook, offers flexibility and a component-based architecture, while Angular, a framework by Google, provides a comprehensive solution with built-in tools and strong typing. The comparison covers performance, learning curve, community support, and use cases to help developers choose the right tool for their projects.",
     url: "/blog/react-vs-angular"
 }
--- src/components/blogpost/BlogPost.tsx
+++ src/components/blogpost/BlogPost.tsx
@@ -8,20 +8,25 @@ type Props = {
 export default function BlogPost({blogPost}: Props) {
     return (
         <article className="blogpost">
-            <h3>
-                {blogPost.title}
-            </h3>
-            <p className="blogpost__author">by {blogPost.author}</p>
+            <a href={blogPost.url}>
+                <h3>
+                    {blogPost.title}
+                </h3>
+            </a>
+            <p className="blogpost__author">
+                by <a href={blogPost.author.url} className="blogpost__author-link">{blogPost.author.name}</a>
+            </p>
             <div className="blogpost__preview-container">
                 {blogPost.image ?
-                    <img className="blogpost__image" src={blogPost.image} alt={blogPost.title} width={400}/> :
+                    <img className="blogpost__image" src={blogPost.image} alt={blogPost.title}/> :
                     <img className="blogpost__image" src="/src/assets/no-image.png" alt="No image available"
-                         width={400}/>}
-                <div className="blogpost__summary">
-                    <p>{blogPost.summary}</p>
-                    <p>
-                        <a href={blogPost.url}>Read more</a>
+                    />}
+                <div className="blogpost__summary-container">
+                    <p className="blogpost__summary">{blogPost.summary}
                     </p>
+                    <a href={blogPost.url} className="blogpost__read-more-btn">Read more</a>
+
+
                 </div>
             </div>
         </article>
